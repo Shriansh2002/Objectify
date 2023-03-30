18763e286b5425194eba8cb09b2593dc318eca9a
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2023-03-30 07:59:26 +0530

    Created AboutPage

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,9 @@
-import { Fragment } from 'react';
-
+import Footer from '@/components/global/Footer';
+import Header from '@/components/global/Header';
 import Head from 'next/head';
+import Link from 'next/link';
+import { Fragment } from 'react';
+import { FaGithub } from 'react-icons/fa';
 
 const AboutPage = () => {
     return (
@@ -9,8 +12,43 @@ const AboutPage = () => {
                 <title>About - Objectify</title>
             </Head>
 
-            <main>
+            <main className="flex flex-col min-h-screen justify-between">
+                <Header />
+                <div className="bg-white">
+                    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                        <div className="max-w-3xl mx-auto py-16 sm:py-24">
+                            <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
+                                About Our {""}
+                                <span className='text-indigo-600'>
+                                    Objectify
+                                </span>
+                            </h2>
+                            <p className="mt-6 text-xl text-gray-500">
+                                Our TensorflowJS Object Detection App uses machine learning algorithms to detect objects from an image. It is built using NextJS and TailwindCSS.
+                            </p>
+                            <p className="mt-6 text-xl text-gray-500">
+                                This application is designed to help users identify objects in images using state-of-the-art machine learning models. Our goal is to make it easy for everyone to leverage the power of machine learning and AI to solve real-world problems.
+                            </p>
+                            <p className="mt-6 text-xl text-gray-500">
+                                To learn more about the technologies we used to build this application, please check out our Github repository:
+                            </p>
+                            <div className="mt-8">
+                                <Link
+                                    href="https://github.com/shriansh2002/objectify"
 
+                                    className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
+                                    rel="noopener noreferrer" target="_blank"
+                                >
+                                    <FaGithub className="mr-2" />
+                                    Github Repository
+
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <Footer />
             </main>
         </Fragment>
     );

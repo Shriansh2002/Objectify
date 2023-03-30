33130b325f8cfbd81efd29a43ff0abe70f1649e2
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2023-03-30 07:53:19 +0530

    About Page

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import { Fragment } from 'react';
+
+import Head from 'next/head';
+
+const AboutPage = () => {
+    return (
+        <Fragment>
+            <Head>
+                <title>About - Objectify</title>
+            </Head>
+
+            <main>
+
+            </main>
+        </Fragment>
+    );
+};
+
+export default AboutPage;

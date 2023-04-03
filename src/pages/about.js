2b903495ca7597288413c80a8a8433dfdc3f925c
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2023-04-03 10:05:06 +0530

    Created About and Contact Page UI

--- src/pages/about.js
+++ src/pages/about.js
@@ -22,7 +22,7 @@ const AboutPage = () => {
                     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                         <div className="max-w-3xl mx-auto py-16 sm:py-24">
                             <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
-                                About Our {""}
+                                About {""}
                                 <span className='text-indigo-600'>
                                     Objectify
                                 </span>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,7 @@ import Footer from '@/components/global/Footer';
 import Header from '@/components/global/Header';
 
 // Icons
-import { FaEnvelope, FaPhone } from "react-icons/fa";
+
 
 const ContactPage = () => {
     return (
@@ -19,21 +19,67 @@ const ContactPage = () => {
             <main className="flex flex-col min-h-screen justify-between">
                 <Header />
 
-                <div className="bg-white dark:bg-gray-800">
-                    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                        <div className="max-w-3xl mx-auto py-16 sm:py-24">
-                            <h2 className="text-3xl font-extrabold text-gray-900 dark:text-white sm:text-4xl">
-                                Contact Us
-                            </h2>
-                            <p className="mt-6 text-xl text-gray-500 dark:text-gray-300">
-                                If you have any questions or feedback, please don{""}t hesitate to get in touch with us. You can contact either of us using the details below:
-                            </p>
 
+                <section className="flex-grow bg-gray-100 py-12">
+                    <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+                        <h2 className="text-3xl font-bold mb-8 text-center">Contact Us</h2>
 
+                        <form className='bg-white mx-auto max-w-2xl shadow-md rounded-lg  px-8 py-12'>
+                            <div className="mb-6">
+                                <label
+                                    for="email"
+                                    className="block mb-2 text-sm font-medium text-gray-900">
+                                    Your Email
+                                </label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                    placeholder="Enter Your Name"
+                                    required
+                                />
+                            </div>
 
-                        </div>
+                            <div className="mb-6">
+                                <label
+                                    for="name"
+                                    className="block mb-2 text-sm font-medium text-gray-900">
+                                    Your Name
+                                </label>
+                                <input
+                                    type="text"
+                                    id="name"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                    placeholder="Enter Your Email"
+                                    required
+                                />
+                            </div>
+
+                            <div className="mb-6">
+                                <label
+                                    for="large-input"
+                                    className="block mb-2 text-sm font-medium text-gray-900"
+                                >
+                                    Your Message
+                                </label>
+                                <textarea
+                                    type="text"
+                                    id="large-input"
+                                    className="block w-full text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 p-2.5"
+                                    placeholder="Enter your message"
+                                />
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded w-full"
+                            >
+                                Send
+                            </button>
+                        </form>
                     </div>
-                </div>
+
+                </section>
 
                 <Footer />
 

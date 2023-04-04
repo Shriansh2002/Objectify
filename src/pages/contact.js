1d6cef348a27b62c70ec33a2edf84da93a0ed84e
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2023-04-04 19:40:37 +0530

    Created Hubspot Form For Contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,28 @@
 import Head from 'next/head';
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 
 // Components
 import Footer from '@/components/global/Footer';
 import Header from '@/components/global/Header';
 
-// Icons
-
 
 const ContactPage = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://js.hsforms.net/forms/v2.js';
+        document.body.appendChild(script);
+
+        script.addEventListener('load', () => {
+            if (window.hbspt) {
+                window.hbspt.forms.create({
+                    portalId: process.env.NEXT_PUBLIC_HUBSPOT_PORTAL_ID,
+                    formId: process.env.NEXT_PUBLIC_HUBSPOT_FORM_ID,
+                    target: '#hubspotForm'
+                });
+            }
+        });
+    }, []);
+
     return (
         <Fragment>
 
@@ -24,59 +38,9 @@ const ContactPage = () => {
                     <div className="container mx-auto px-4 sm:px-6 lg:px-8">
                         <h2 className="text-3xl font-bold mb-8 text-center">Contact Us</h2>
 
-                        <form className='bg-white mx-auto max-w-2xl shadow-md rounded-lg  px-8 py-12'>
-                            <div className="mb-6">
-                                <label
-                                    htmlFor="email"
-                                    className="block mb-2 text-sm font-medium text-gray-900">
-                                    Your Email
-                                </label>
-                                <input
-                                    type="email"
-                                    id="email"
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5"
-                                    placeholder="Enter Your Name"
-                                    required
-                                />
-                            </div>
+                        <div id="hubspotForm"></div>
 
-                            <div className="mb-6">
-                                <label
-                                    htmlFor="name"
-                                    className="block mb-2 text-sm font-medium text-gray-900">
-                                    Your Name
-                                </label>
-                                <input
-                                    type="text"
-                                    id="name"
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5"
-                                    placeholder="Enter Your Email"
-                                    required
-                                />
-                            </div>
 
-                            <div className="mb-6">
-                                <label
-                                    htmlFor="large-input"
-                                    className="block mb-2 text-sm font-medium text-gray-900"
-                                >
-                                    Your Message
-                                </label>
-                                <textarea
-                                    type="text"
-                                    id="large-input"
-                                    className="block w-full text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-indigo-500 focus:border-indigo-500 p-2.5"
-                                    placeholder="Enter your message"
-                                />
-                            </div>
-
-                            <button
-                                type="submit"
-                                className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded w-full"
-                            >
-                                Send
-                            </button>
-                        </form>
                     </div>
 
                 </section>

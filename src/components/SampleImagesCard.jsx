53a268d35e8f474076cf0e96d1bcddeb0b5f6cdd
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2023-04-05 11:05:48 +0530

    SampleImagesCard UI

--- src/components/SampleImagesCard.jsx
+++ src/components/SampleImagesCard.jsx
@@ -3,17 +3,25 @@ import React from 'react';
 const SampleImagesCard = ({ image, _idx, handleSampleImageChange }) => {
 	return (
 		<div
-			className="max-w-sm rounded-lg overflow-hidden shadow-lg cursor-pointer hover:shadow-2xl"
+			className="max-w-sm rounded-lg overflow-hidden shadow-lg hover:shadow-2xl"
 			key={_idx}
-			onClick={() => handleSampleImageChange(_idx)}
 		>
 			<img
 				src={image.url}
-				alt={image.name}
-				className="w-full cursor-pointer h-52"
+				alt="Loading..."
+				className="w-full h-52 cursor-pointer"
 			/>
-			<div className="px-6 pt-2 pb-1">
-				<h2 className="font-bold text-xl mb-2">{image.name}</h2>
+			<div className="px-6 py-4">
+				<button
+					target="_blank"
+					rel="noreferrer"
+					className="w-full relative inline-flex items-center justify-center py-2 px-4 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded shadow-xl group hover:ring-1 hover:ring-purple-500"
+					onClick={() => handleSampleImageChange(_idx)}
+				>
+					<span className="absolute inset-0 w-full h-full bg-gradient-to-br from-indigo-600 via-purple-600 to-pink-700"></span>
+					<span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded opacity-30 group-hover:rotate-90 ease"></span>
+					<span className="relative text-white">Select</span>
+				</button>
 			</div>
 		</div>
 	);
